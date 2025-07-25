8e2a31ed0a09eb306502c98833d67e3dbaf1d598
pandyandy <and.reanovakova@seznam.cz>
2025-07-25 12:53:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from visuals.g2 import g2
 from visuals.reddit import  reddit
 from visuals.capterra import capterra
 from visuals.category_colors import categorize_sentiment
+from keboola_streamlit import KeboolaStreamlit
 
 st.set_page_config(layout='wide')
 
@@ -28,16 +29,24 @@ st.markdown(f"{logo_html}", unsafe_allow_html=True)
 # Caption
 st.caption("<br>See how users rate Keboola on various platforms & social media.<br>", unsafe_allow_html=True)
 
-# Data
-g2_data = pd.read_csv(st.secrets['g2_sentiment'])
-g2_keywords = pd.read_csv(st.secrets['g2_keywords'])
 
-reddit_data = pd.read_csv(st.secrets['reddit_sentiment'])
-reddit_keywords = pd.read_csv(st.secrets['reddit_keywords'])
+@st.cache_data
+def load_data():
+    keboola = KeboolaStreamlit(
+        st.secrets["kbc_url"],
+        st.secrets["kbc_token"],
 
-capterra_data = pd.read_csv(st.secrets['capterra_sentiment'])
-capterra_keywords = pd.read_csv(st.secrets['capterra_keywords'])
+        
+    )
+    g2_data = keboola.read_table("out.c-sentiment-cleaning.g2_sentiment_final")
+    g2_keywords = keboola.read_table("out.c-keywords-cleaning.g2_keywords_final")
+    reddit_data = keboola.read_table("out.c-sentiment-cleaning.reddit_sentiment_final")
+    reddit_keywords = keboola.read_table("out.c-keywords-cleaning.reddit_keywords_final")
+    capterra_data = keboola.read_table("out.c-sentiment-cleaning.capterra_sentiment_final")
+    capterra_keywords = keboola.read_table("out.c-keywords-cleaning.capterra_keywords_final")
+    return g2_data, g2_keywords, reddit_data, reddit_keywords, capterra_data, capterra_keywords
 
+g2_data, g2_keywords, reddit_data, reddit_keywords, capterra_data, capterra_keywords = load_data()
 
 tab1, tab2, tab3 = st.tabs(["G2", "Capterra", "Reddit"])
 with tab1:
